8003b260083435ca95a0972aeb624f0070a641de
azat <azotmuhamed@gmail.com>
2021-07-11 21:14:00 +0300

    code

--- traffic lights/ViewController.swift
+++ traffic lights/ViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+   
+    enum currentLight {
+        case red, yellow, green
+    }
     
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
@@ -15,11 +21,33 @@ class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
+    var currentLight = currentLight.red
+    
+    let lightOn: CGFloat = 1
+    let lightOf: CGFloat = 0.3
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = lightOf
+        yellowLight.alpha = lightOn
+        greenLight.alpha = lightOf
+        
+        startButton.layer.cornerRadius = 15
+        
     }
 
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
 
+    @IBAction func startButtonPress() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight
+    }
 }
 
