f1491324ec97cfbe1dc6be88749a08567ceb4a66
azat <azotmuhamed@gmail.com>
2021-07-11 19:24:00 +0300

    Initial Commit

---
+++ traffic lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic lights
+//
+//  Created by Азат Мухамедов on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
